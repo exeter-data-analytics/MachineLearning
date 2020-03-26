ee0af0da87f1e228f806b1ed6e2ddc19d92b55d4
swgeoscience <cmy203@GWatts-Air.lan>
2020-03-26 09:20:20 +0000

    minor amendment to website

--- _build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-
-## copy nojekyll file and data
-cp .nojekyll _site/ 
-zip -r data.zip _data 
-cp -r data.zip _site/
-cp -r slides.zip _site/
